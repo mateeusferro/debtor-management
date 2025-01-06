995ce892a23c9e465267821d34149e87ec617728
mateus <mateus.03fernandesaraujo@gmail.com>
2025-01-06 19:46:58 -0300

    feat: add simple test on main

--- src/test/java/com/ferromateus/debtorsmanagement/DebtorsManagementApplicationTests.java
+++ src/test/java/com/ferromateus/debtorsmanagement/DebtorsManagementApplicationTests.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class DebtorsManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		DebtorsManagementApplication debtorsManagementApplication = new DebtorsManagementApplication();
+		assertNotNull(debtorsManagementApplication);
 	}
 
 }
