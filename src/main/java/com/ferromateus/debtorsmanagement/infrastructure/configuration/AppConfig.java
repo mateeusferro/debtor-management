2439374f2b020fa899ab8ef4093afe55a47f6ee5
mateus <mateus.03fernandesaraujo@gmail.com>
2025-01-07 20:07:43 -0300

    feat: add beans for new class

--- src/main/java/com/ferromateus/debtorsmanagement/infrastructure/configuration/AppConfig.java
+++ src/main/java/com/ferromateus/debtorsmanagement/infrastructure/configuration/AppConfig.java
@@ -2,16 +2,22 @@ package com.ferromateus.debtorsmanagement.infrastructure.configuration;
 
 import com.ferromateus.debtorsmanagement.application.usecase.debt.impl.CreateDebtUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.debt.impl.GetDebtByIdUseCaseImpl;
+import com.ferromateus.debtorsmanagement.application.usecase.debt.impl.GetDebtsUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.debt.interfaces.CreateDebtUseCase;
 import com.ferromateus.debtorsmanagement.application.usecase.debt.interfaces.GetDebtByIdUseCase;
+import com.ferromateus.debtorsmanagement.application.usecase.debt.interfaces.GetDebtsUseCase;
 import com.ferromateus.debtorsmanagement.application.usecase.debtor.impl.CreateDebtorUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.debtor.impl.GetDebtorByIdUseCaseImpl;
+import com.ferromateus.debtorsmanagement.application.usecase.debtor.impl.GetDebtorsUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.debtor.interfaces.CreateDebtorUseCase;
 import com.ferromateus.debtorsmanagement.application.usecase.debtor.interfaces.GetDebtorByIdUseCase;
+import com.ferromateus.debtorsmanagement.application.usecase.debtor.interfaces.GetDebtorsUseCase;
 import com.ferromateus.debtorsmanagement.application.usecase.payment.impl.CreatePaymentUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.payment.impl.GetPaymentByIdUseCaseImpl;
+import com.ferromateus.debtorsmanagement.application.usecase.payment.impl.GetPaymentsUseCaseImpl;
 import com.ferromateus.debtorsmanagement.application.usecase.payment.interfaces.CreatePaymentUseCase;
 import com.ferromateus.debtorsmanagement.application.usecase.payment.interfaces.GetPaymentByIdUseCase;
+import com.ferromateus.debtorsmanagement.application.usecase.payment.interfaces.GetPaymentsUseCase;
 import com.ferromateus.debtorsmanagement.domain.gateway.DebtGateway;
 import com.ferromateus.debtorsmanagement.domain.gateway.DebtorGateway;
 import com.ferromateus.debtorsmanagement.domain.gateway.PaymentGateway;
@@ -53,6 +59,11 @@ public class AppConfig {
     }
 
     @Bean
+    GetDebtorsUseCase getDebtorsUseCase(DebtorGateway debtorGateway) {
+        return new GetDebtorsUseCaseImpl(debtorGateway);
+    }
+
+    @Bean
     CreateDebtUseCase createDebtUseCase(DebtGateway debtGateway, DebtorGateway debtorGateway) {
         return new CreateDebtUseCaseImpl(debtGateway, debtorGateway);
     }
@@ -63,6 +74,11 @@ public class AppConfig {
     }
 
     @Bean
+    GetDebtsUseCase getDebtsUseCase(DebtGateway debtGateway) {
+        return new GetDebtsUseCaseImpl(debtGateway);
+    }
+
+    @Bean
     CreatePaymentUseCase createPaymentUseCase(PaymentGateway paymentGateway, DebtGateway debtGateway) {
         return new CreatePaymentUseCaseImpl(paymentGateway, debtGateway);
     }
@@ -70,6 +86,11 @@ public class AppConfig {
     @Bean
     GetPaymentByIdUseCase getPaymentByIdUseCase(PaymentGateway paymentGateway) {
         return new GetPaymentByIdUseCaseImpl(paymentGateway);
+    }
+
+    @Bean
+    GetPaymentsUseCase getPaymentsUseCase(PaymentGateway paymentGateway) {
+        return new GetPaymentsUseCaseImpl(paymentGateway);
     }
 
 }
